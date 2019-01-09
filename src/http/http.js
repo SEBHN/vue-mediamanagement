a24a3596f82c3252c1d5f1560a0151e37346f05c
Emin Korkmaz <korkmaz.emin@hotmail.de>
2019-01-09 20:28:00 +0100

    fix baseUrl

--- src/http/http.js
+++ src/http/http.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 // REST api : 
-const baseUrl = `https://mvs-18-ws-spring-in-cloud.appspot.com`
+//const baseUrl = `https://mvs-18-ws-spring-in-cloud.appspot.com`; => this version of backend needs auth
+const baseUrl = 'http://localhost:8080/';
 
 export let http = axios.create({
   baseURL: baseUrl
-})
+});
