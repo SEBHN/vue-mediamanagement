a9ef814ad0ffa313542544b1d43434cc6df48df0
Manuel <wtfjoke@livenet.ch>
2019-01-17 11:18:16 +0100

    Use only necessary suffix

--- src/main.js
+++ src/main.js
@@ -8,13 +8,12 @@ import 'vuetify/dist/vuetify.min.css'
 import { routes } from './routes'
 
 
-const production = process.env.NODE_ENV === 'production' ? 'https://sebhn.github.io/mvsvue/': 'http://localhost:8080/'
-const mode =  process.env.NODE_ENV === 'production' ? 'history': 'history'
+const productionSuffix = process.env.NODE_ENV === 'production' ? '/mvsvue/': '/'
 Vue.use(VueRouter);
 Vue.use(OktaVuePlugin, {
   issuer: 'https://dev-332680.oktapreview.com/oauth2/default',
   client_id: '0oahyyghlrI8poUre0h7',
-  redirect_uri: production + 'implicit/callback',
+  redirect_uri:  window.location.origin + productionSuffix + 'implicit/callback',
   scope: 'openid profile email',
 });
 
@@ -22,7 +21,7 @@ Vue.use(OktaVuePlugin, {
 const router = new VueRouter({
   routes, // pass in our imported routes array
   base: process.env.BASE_URL,
-  mode: mode // remove '/#' (hash mode)npm
+  mode: 'history' // remove '/#' (hash mode)npm
 });
 
 router.beforeEach(Vue.prototype.$auth.authRedirectGuard());
